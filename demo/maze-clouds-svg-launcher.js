9305cd7dc6ea648736f64069fbcf0f1c130467cf
Chris Pressey <cpressey@catseye.tc>
2019-05-19 21:40:23 +0100

    Re-arrange the control panel.

--- demo/maze-clouds-svg-launcher.js
+++ demo/maze-clouds-svg-launcher.js
@@ -52,70 +52,76 @@ function launch(config) {
 
   var div=DAM.maker('div'), button=DAM.maker('button');
   var controlPanel = div(
-    button("Re-roll", { onclick: function() { maze = makeMaze(); redrawMaze(); }}),
-    DAM.makeRange({ title: "Hue:", min: 0, max: 359, value: 180, onchange: function(v) { hue = v; redrawMaze(); } }),
-    DAM.makeSelect({
-      title: "Visuals",
-      options: [
-        {
-          text: 'Cumulus',
-          value: 1,
-          obtainColour: function(x, y, depth) {
-            var l = ((depth / maze.maxValue) / 2) + 0.5;
-            var s = 1.0;
-            return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
+    div(
+      DAM.makeRange({ title: "Hue:", min: 0, max: 359, value: 180, onchange: function(v) { hue = v; redrawMaze(); } })
+    ),
+    div(
+      DAM.makeSelect({
+        title: "Visuals",
+        options: [
+          {
+            text: 'Cumulus',
+            value: 1,
+            obtainColour: function(x, y, depth) {
+              var l = ((depth / maze.maxValue) / 2) + 0.5;
+              var s = 1.0;
+              return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
+            }
+          },
+          {
+            text: 'Overcast',
+            value: 2,
+            obtainColour: function(x, y, depth) {
+              var l = 0.5;
+              var s = depth / maze.maxValue;
+              return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
+            }
+          },
+          {
+            text: 'Stormy',
+            value: 3,
+            obtainColour: function(x, y, depth) {
+              var l = depth / maze.maxValue;
+              var s = 1.0;
+              return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
+            }
+          },
+          {
+            text: 'Quite Stormy',
+            value: 4,
+            obtainColour: function(x, y, depth) {
+              var l = depth / maze.maxValue;
+              var s = depth / maze.maxValue;
+              return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
+            }
+          },
+          {
+            text: 'Rainbow',
+            value: 5,
+            obtainColour: function(x, y, depth) {
+              var l = 0.5;
+              var s = 1.0;
+              var h = Math.trunc(depth / maze.maxValue * 360);
+              return "hsl(" + h + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
+            }
+          },
+          {
+            text: 'Digital',
+            value: 6,
+            obtainColour: function(x, y, depth) {
+              return (depth % 3 === 0) ? "#000000" : "#ffffff";
+            }
           }
-        },
-        {
-          text: 'Overcast',
-          value: 2,
-          obtainColour: function(x, y, depth) {
-            var l = 0.5;
-            var s = depth / maze.maxValue;
-            return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
-          }
-        },
-        {
-          text: 'Stormy',
-          value: 3,
-          obtainColour: function(x, y, depth) {
-            var l = depth / maze.maxValue;
-            var s = 1.0;
-            return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
-          }
-        },
-        {
-          text: 'Quite Stormy',
-          value: 4,
-          obtainColour: function(x, y, depth) {
-            var l = depth / maze.maxValue;
-            var s = depth / maze.maxValue;
-            return "hsl(" + hue + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
-          }
-        },
-        {
-          text: 'Rainbow',
-          value: 5,
-          obtainColour: function(x, y, depth) {
-            var l = 0.5;
-            var s = 1.0;
-            var h = Math.trunc(depth / maze.maxValue * 360);
-            return "hsl(" + h + "," + Math.trunc(s * 100) + "%," + Math.trunc(l * 100) + "%)";
-          }
-        },
-        {
-          text: 'Digital',
-          value: 6,
-          obtainColour: function(x, y, depth) {
-            return (depth % 3 === 0) ? "#000000" : "#ffffff";
-          }
+        ],
+        onchange: function(option) {
+          obtainColour = option.obtainColour;
+          redrawMaze();
         }
-      ],
-      onchange: function(option) {
-        obtainColour = option.obtainColour;
-        redrawMaze();
-      }
-    })
+      })
+    ),
+    div(
+      button("Re-roll", { onclick: function() { maze = makeMaze(); redrawMaze(); }})
+    )
   );
   config.container.appendChild(controlPanel);
 
